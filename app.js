fcc60f0eb6e8182d9e418a49ba06c8dae8c983af
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-03-20 13:44:08 -0700

    Connect database

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect")
+require("dotenv").config();
 
+const port = process.env.PORT || 3000;
+const host = process.env.HOST;
+
+
+app.use("/", (req, res)=>{
+    res.send("<h1>Volunteers R Us</h1>");
+});
+
+
+
+const start = async () => {
+    try {
+        //connect DB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on ${host} port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
