703fbe60460ff4dfb87b3590cf7e7d7493400072
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-03-21 21:55:50 -0700

    edit volunteer getOneVolunteer route

--- routes/volunteers.js
+++ routes/volunteers.js
@@ -5,6 +5,6 @@ const volunteerController = require("../controllers/volunteers");
 
 console.log("loading volunteer route");
 router.get("/", volunteerController.getAllVolunteers);
-router.get("/:id", volunteerController.getAllVolunteers);
+router.get("/:id", volunteerController.getOneVolunteer);
 
 module.exports = router;
--- swagger.js
+++ swagger.js
@@ -5,10 +5,13 @@ const doc = {
     title: "My API",
     description: "API Documentation for Contacts Project Week 2",
   },
-
+  host: "",
   servers: [
     { url: "http://localhost:5150", description: "Local Server" },
-    // { url: "https://cse341-contacts-dqby.onrender.com", description: "Render Server" }
+    {
+      url: "https://cse341-contacts-dqby.onrender.com",
+      description: "Render Server",
+    },
   ],
   schemes: ["http", "https"],
 };
