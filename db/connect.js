512b45957de08ce23018fb9b3bbce27656d900fc
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-03-27 19:34:51 -0700

    remove spaces

--- db/connect.js
+++ db/connect.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    serverSelectionTimeoutMS: 5000
-  }).catch(err => console.log("error", err.reason));  //should come back as Authentication error with no password, but shows "undefined"
+  return mongoose
+    .connect(url, {
+      serverSelectionTimeoutMS: 5000,
+    })
+    .catch((err) => console.log("error", err.reason)); //should come back as Authentication error with no password, but shows "undefined"
 };
 
 module.exports = connectDB;
 
-
 /**
- * Another option is 
+ * Another option is
  * const connectDB = async (url) => {
  *   try {
  *      const connection = await mongoose.connect(url)
- *      
+ *
  *   } catch (err) {
  *     console.log(err)
  *     process.exit(1)  // 1 is for exit with failure
